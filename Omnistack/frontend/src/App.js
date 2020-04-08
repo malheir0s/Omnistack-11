9b7a724a7d883bc9d4aba55747fdcf33e4f6e58f
malheir0s <dmalheiros77@gmail.com>
2020-04-08 15:49:01 -0300

    Frontend completo

--- Omnistack/frontend/src/App.js
+++ Omnistack/frontend/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { useState } from 'react';
+
+//import Header from './Header';
+//import Logon from './pages/Logon'
+
+import Routes from './routes';
+
+
+import './global.css';
+
+//Array [valor, funcaoDeAtualizacao]
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World React.js!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  return(
+    <Routes/>
   );
+
+  /*
+  const [counter, setCounter] = useState(0);
+
+  function increment(){
+    setCounter (counter + 1);
+  }
+
+  return (
+    <div>
+     <Header>Contador: {counter} </Header>
+     <button onClick={increment}>Incrementar!</button>
+    </div>
+  );*/
 }
 
 export default App;
